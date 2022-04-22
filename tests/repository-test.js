ee36f273a2c1823cfce0d21c21283f0ba861d690
gabriel-capta <gabriel78santos@gmail.com>
2022-04-22 11:56:56 -0300

    test mail account

--- tests/repository-test.js
+++ tests/repository-test.js
@@ -12,3 +12,37 @@ var readTest = async function (){
     console.log(entity );
 
 }();
+
+var intialMailAccount = async function (){
+
+    //const connectionString = process.env.MONGODB_CONNECTION || "mongodb+srv://add_admin:Addvisor2020@cluster0.jex0n.mongodb.net/add";
+    const connectionString = process.env.MONGODB_CONNECTION || "mongodb+srv://admin:Capta2022@cluster0.zvrhh.mongodb.net/capta";
+    const options = {client: 'mongodb', connectionString:connectionString, debug:true, collection:'mail.account'}
+
+    const repository = new Repository(options);
+    const account = {
+        account: "tecban@addvisor.com.br",
+        password: "Addvisor098!",
+        protocol: "IMAP",
+        host: "email-ssl.com.br",
+        port: 993,
+        ssl: true,
+        listener: true,
+        enable: true,
+    };
+    /*
+    const account = {
+        account: "apresent.csc@addvisor.com.br",
+        password: "Addvisor@2017",
+        protocol: "IMAP",
+        host: "email-ssl.com.br",
+        port: 993,
+        ssl: true,
+        listener: true,
+        enable: true,
+    };
+    */
+    const entity =  await repository.insert(account);
+    console.log("Mail Account", entity);
+
+} //Não rodar
