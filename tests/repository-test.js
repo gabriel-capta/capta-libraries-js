cad1ea3ea5b9ae9cc2f16d7baa6bd8ca83a9ea82
gabriel-capta <gabriel78santos@gmail.com>
2022-04-25 22:20:37 -0300

    local logger

--- tests/repository-test.js
+++ tests/repository-test.js
@@ -1,15 +1,18 @@
-const {Repository} = require('../lib/capta-library.js');
+const {Repository, Logger} = require('../lib/capta-library.js');
 
 var readTest = async function (){
 
     //const connectionString = process.env.MONGODB_CONNECTION || "mongodb+srv://add_admin:Addvisor2020@cluster0.jex0n.mongodb.net/add";
-    const connectionString = process.env.MONGODB_CONNECTION || "mongodb+srv://admin:Capta2022@cluster0.zvrhh.mongodb.net/capta";
-    const options = {client: 'mongodb', connectionString:connectionString, debug:true, collection:'test-collection'}
+    //const connectionString = process.env.MONGODB_CONNECTION || "mongodb+srv://admin:Capta2022@cluster0.zvrhh.mongodb.net/capta";
+    //const options = {client: 'mongodb', connectionString:connectionString, debug:true, collection:'test-collection'}
 
     const repository = new Repository();
     const entity =  await repository.collection('test-collection').insert({test: 'capta-libraries-js', date: new Date()});
     //const user = await repository.get('605cd51522606db9ca650e64');
-    console.log(entity );
+
+    const logger = new Logger();
+    logger.log(entity);
+    
 
 }();
 
