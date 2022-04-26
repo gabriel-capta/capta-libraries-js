3aea63dab21e06d52752c3fef1b671e8862a6502
gabriel-capta <gabriel78santos@gmail.com>
2022-04-25 21:04:23 -0300

    constructor read properties

--- tests/repository-test.js
+++ tests/repository-test.js
@@ -6,8 +6,8 @@ var readTest = async function (){
     const connectionString = process.env.MONGODB_CONNECTION || "mongodb+srv://admin:Capta2022@cluster0.zvrhh.mongodb.net/capta";
     const options = {client: 'mongodb', connectionString:connectionString, debug:true, collection:'test-collection'}
 
-    const repository = new Repository(options);
-    const entity =  await repository.insert({test: 'capta-libraries-js', date: new Date()});
+    const repository = new Repository();
+    const entity =  await repository.collection('test-collection').insert({test: 'capta-libraries-js', date: new Date()});
     //const user = await repository.get('605cd51522606db9ca650e64');
     console.log(entity );
 
